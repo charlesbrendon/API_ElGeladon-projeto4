f89f82eb2b871c5b0518e52096dcaecc6885e232
Charles Brendon <charlesbrendonss@gmail.com>
2022-06-13 22:47:15 -0300

    main

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 // importando app
 import app from './app';
 //importando banco de dados
-import { conectarAoDatabase } from './database';
+import { conectarAoDatabase } from './database/index.js';
 
 const port = process.env.PORT || 3003;
 
 app.listen(port, () => {
   conectarAoDatabase();
+  if(!process.env.PORT)
     console.log(`A aplicação está rodando na porta http://localhost:${port}`);
   });
